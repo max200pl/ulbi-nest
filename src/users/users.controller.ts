75f0e6694058314c5a464eedad4924d771cda624
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-07-13 22:53:43 +0300

    added contoller user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { CreateUserDto } from "./dto/create-user.dto";
+import { Body } from "@nestjs/common";
+import { Post } from "@nestjs/common";
+import { Controller } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { Get } from "@nestjs/common";
 
-@Controller('users')
-export class UsersController {}
+@Controller("users")
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this.usersService.createUser(userDto);
+  }
+
+  @Get()
+  getAll() {
+    return this.usersService.getAllUsers();
+  }
+}
