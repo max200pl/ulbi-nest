cb68a84f142fe5ea8dac3a609f49d5391ee90f38
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-07-13 19:54:43 +0300

    fix bug

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,7 @@ import { User } from "./users.model";
 import { InjectModel } from "@nestjs/sequelize";
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User) private userRepository: typof User) {}
+  constructor(@InjectModel(User) private userRepository: typeof User) {}
   async createUser() {}
   async getAllUsers() {}
 }
