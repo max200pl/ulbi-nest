50f29b48ab5bdaf5fbe8f9055f4231ec036c62f0
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-07-13 23:19:23 +0300

    added documentation swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,24 @@
+import { User } from "./users.model";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { Body } from "@nestjs/common";
 import { Post } from "@nestjs/common";
 import { Controller } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { Get } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Пользователи")
 @Controller("users")
 export class UsersController {
   constructor(private usersService: UsersService) {}
+  @ApiOperation({ summary: "создание пользователя" })
+  @ApiResponse({ status: 200, type: User })
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.usersService.createUser(userDto);
   }
-
+  @ApiOperation({ summary: "получить всех пользователей" })
+  @ApiResponse({ status: 200, type: [User] })
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 
 //  generic
@@ -10,6 +11,7 @@ interface UserCreationAttrs {
 //  generic
 @Table({ tableName: "users" })
 export class User extends Model<User> {
+  @ApiProperty({ example: "1", description: "Уникальный идентификатор " })
   @Column({
     type: DataType.INTEGER, // тип поля INTEGER
     unique: true, // поле должно быть уникальным
@@ -17,25 +19,44 @@ export class User extends Model<User> {
     primaryKey: true, // первичный ключ
   })
   id: number;
-
+  @ApiProperty({
+    example: "user@mail.ru",
+    description: "Почтовый ящик",
+  })
   @Column({
     type: DataType.STRING, // тип поля STRING
     unique: true,
     allowNull: false, // не должен инкриминироваться
   })
   email: string;
-
+  @ApiProperty({
+    example: "12345",
+    description: "Пароль пользователя",
+  })
   @Column({
     type: DataType.BOOLEAN, // тип поля STRING
     unique: true,
     defaultValue: false, // пользователь изначально не забанен
   })
   password: string;
-
+  @ApiProperty({
+    example: "true",
+    description: "Забанен пользователь или нет",
+  })
   @Column({
     type: DataType.BOOLEAN, // тип поля STRING
     unique: true,
     allowNull: false, // пользователь изначально не забанен
   })
   banned: boolean;
+  @ApiProperty({
+    example: "За хулиганство",
+    description: "Причина блокировки",
+  })
+  @Column({
+    type: DataType.BOOLEAN, // тип поля STRING
+    unique: true,
+    allowNull: true, // пользователь изначально не забанен
+  })
+  banReason: string;
 }
