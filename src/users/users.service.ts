3c06a53b1b0bb06d6e28f49c1af1ffbc852b9bad
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-07-13 20:00:00 +0300

    added sevice

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,15 @@
+import { CreateUserDto } from "./dto/create-user.dto";
 import { Injectable } from "@nestjs/common";
 import { User } from "./users.model";
 import { InjectModel } from "@nestjs/sequelize";
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User) private userRepository: typeof User) {}
-  async createUser() {}
-  async getAllUsers() {}
+  async createUser(dto: CreateUserDto) {
+    const user = await this.userRepository.create(dto);
+    return user;
+  }
+  async getAllUsers() {
+    const users = await this.userRepository.findAll();
+  }
 }
